b9f0a9408f35a7796c4ed45b0cd0b430e89ebaa8
yingwai <nick.yingwai@mail.ru>
2024-03-07 14:29:20 +0300

    HW 1

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { HW1 } from './HW/1/HW1';
+
+function App() {
+  return (
+    <div className="App">
+      <HW1 />
+      {/*<HW2 />*/}
+      {/*<HW3 />*/}
+      {/*<HW4 />*/}
+    </div>
+  );
+}
+
+export default App;
