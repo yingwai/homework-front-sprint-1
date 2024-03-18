9544a495fcff9d5f914deb56f3a0050608fe1196
yingwai <nick.yingwai@mail.ru>
2024-03-18 13:10:46 +0300

    HW 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css';
 import { HW1 } from './HW/1/HW1';
 import { HW2 } from './HW/2/HW2';
+import { HW3 } from './HW/3/HW3';
 
 function App() {
   return (
     <div className="App">
       <HW1 />
       <HW2 />
-      {/*<HW3 />*/}
+      <HW3 />
       {/*<HW4 />*/}
     </div>
   );
